b3846d9600dee7b0e83bdbb4cf0bde362b37b954
AtliOskarsson <atlio@outlook.com>
2018-03-16 10:08:56 +0000

    fyrsta gerð af asteroid leik

---
+++ leikur.js
@@ -0,0 +1,107 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var img;
+
+var width = canvas.width = window.innerWidth -25;
+var height = canvas.height = window.innerHeight - 100;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+
+var image = new Image();
+image.src = 'asteroid.png'
+
+// define Ball constructor
+
+function Ball(x, y, velX, velY, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.size = size;
+}
+
+// define ball draw method
+
+Ball.prototype.draw = function() {
+  ctx.drawImage(image, this.x, this.y, this.size*2, this.size*2)
+  ctx.beginPath();
+  ctx.fill();
+};
+
+// define ball update method
+
+Ball.prototype.update = function() {
+  if((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// define ball collision detection
+
+Ball.prototype.collisionDetect = function() {
+  for(var j = 0; j < balls.length; j++) {
+    if(!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        //balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+      }
+    }
+  }
+};
+
+// define array to store balls
+
+var balls = [];
+
+// define loop that keeps drawing the scene constantly
+
+function loop() {
+  ctx.fillStyle = 'rgba(0,191,255,1)';
+  ctx.fillRect(0,0,width,height);
+
+  while(balls.length < 25) {
+    var ball = new Ball(
+      random(0,width),
+      random(0,height),
+      random(-7,7),
+      random(-7,7),
+      random(10,20)
+    );
+    balls.push(ball);
+  }
+
+  for(var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+
+loop();
