e8a9a83ad8bcc5ac1b49a43aa9757b9d29120ab5
AtliOskarsson <atlio@outlook.com>
2018-03-21 15:13:07 +0000

    Images, spawn rate

--- leikur.js
+++ leikur.js
@@ -1,34 +1,39 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-var img;
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let img;
 
-var width = canvas.width = window.innerWidth -25;
-var height = canvas.height = window.innerHeight - 100;
+let width = canvas.width = window.innerWidth - 13;
+let height = canvas.height = window.innerHeight - 20;
 
 // function to generate random number
 
 function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
+  let num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
 
-var image = new Image();
-image.src = 'asteroid.png'
+let image = new Image();
+image.src = 'images/asteroidmirror.png'
+let earth = new Image();
+earth.src = 'images/player-earth.png';
+let jupiter = new Image();
+jupiter.src = 'images/jupiter.png'
 
 // define Ball constructor
 
-function Ball(x, y, velX, velY, size) {
+function Ball(x, y, velX, velY, size, eyda) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
   this.size = size;
+  this.eyda = eyda;
 }
 
 // define ball draw method
 
 Ball.prototype.draw = function() {
-  ctx.drawImage(image, this.x, this.y, this.size*2, this.size*2)
+  ctx.drawImage(image, this.x-15, this.y-15, this.size*2, this.size*2)
   ctx.beginPath();
   ctx.fill();
 };
@@ -38,18 +43,22 @@ Ball.prototype.draw = function() {
 Ball.prototype.update = function() {
   if((this.x + this.size) >= width) {
     this.velX = -(this.velX);
+    this.eyda = 1;
   }
 
-  if((this.x - this.size) <= 0) {
+  if((this.x - this.size) <= -17) {
     this.velX = -(this.velX);
+    this.eyda = 1;
   }
 
   if((this.y + this.size) >= height) {
     this.velY = -(this.velY);
+    this.eyda = 1;
   }
 
   if((this.y - this.size) <= 0) {
     this.velY = -(this.velY);
+    this.eyda = 1;
   }
 
   this.x += this.velX;
@@ -59,11 +68,11 @@ Ball.prototype.update = function() {
 // define ball collision detection
 
 Ball.prototype.collisionDetect = function() {
-  for(var j = 0; j < balls.length; j++) {
+  for(let j = 0; j < balls.length; j++) {
     if(!(this === balls[j])) {
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
-      var distance = Math.sqrt(dx * dx + dy * dy);
+      let dx = this.x - balls[j].x;
+      let dy = this.y - balls[j].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < this.size + balls[j].size) {
         //balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
@@ -74,29 +83,30 @@ Ball.prototype.collisionDetect = function() {
 
 // define array to store balls
 
-var balls = [];
+let balls = [];
 
 // define loop that keeps drawing the scene constantly
 
 function loop() {
-  ctx.fillStyle = 'rgba(0,191,255,1)';
+  ctx.fillStyle = 'rgba(0,0,0,1)';
   ctx.fillRect(0,0,width,height);
-
+  ctx.drawImage(jupiter, 30, 50)
   while(balls.length < 25) {
-    var ball = new Ball(
+    let ball = new Ball(
       random(0,width),
-      random(0,height),
-      random(-7,7),
-      random(-7,7),
-      random(10,20)
+      20,
+      random(-5,5),
+      random(1,7),
+      random(9,19),
     );
     balls.push(ball);
   }
-
-  for(var i = 0; i < balls.length; i++) {
+  for(let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].eyda === 1){
+      balls.splice(i, 1)
+    };
   }
 
   requestAnimationFrame(loop);
