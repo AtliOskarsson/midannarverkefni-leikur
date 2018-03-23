e5d95779ed39338bb234ecb8c3d474d00d2f74f2
AtliOskarsson <atlio@outlook.com>
2018-03-23 10:23:06 +0000

    jamn

--- leikur.js
+++ leikur.js
@@ -1,6 +1,7 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 let img;
+let max = 0;
 
 let width = canvas.width = window.innerWidth - 13;
 let height = canvas.height = window.innerHeight - 20;
@@ -18,6 +19,25 @@ let earth = new Image();
 earth.src = 'images/player-earth.png';
 let jupiter = new Image();
 jupiter.src = 'images/jupiter.png'
+
+function Player(x, y, vel) {
+  this.x = x;
+  this.y = y;
+  this.vel = 0;
+}
+
+Player.prototype.draw = function() {
+  ctx.drawImage(earth, this.x, this.y, 35, 35);
+  ctx.beginPath();
+  ctx.fill();
+};
+
+Player.prototype.update = function() {
+
+  this.x += 0.1;
+  this.y += 0.1;
+  console.log(this.x)
+}
 
 // define Ball constructor
 
@@ -42,22 +62,18 @@ Ball.prototype.draw = function() {
 
 Ball.prototype.update = function() {
   if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
     this.eyda = 1;
   }
 
   if((this.x - this.size) <= -17) {
-    this.velX = -(this.velX);
     this.eyda = 1;
   }
 
   if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
     this.eyda = 1;
   }
 
   if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
     this.eyda = 1;
   }
 
@@ -82,25 +98,30 @@ Ball.prototype.collisionDetect = function() {
 };
 
 // define array to store balls
-
 let balls = [];
 
 // define loop that keeps drawing the scene constantly
+man = new Player(150, 150, 50)
 
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,1)';
   ctx.fillRect(0,0,width,height);
-  ctx.drawImage(jupiter, 30, 50)
-  while(balls.length < 25) {
+  ctx.drawImage(jupiter, 30, 50);
+  ctx.font = '90px Algerian';
+  ctx.fillStyle = 'green';
+  ctx.fillText(Math.round(max, 1), width-150, 70)
+  while(balls.length < max) {
     let ball = new Ball(
       random(0,width),
       20,
-      random(-5,5),
+      random(-3,4),
       random(1,7),
       random(9,19),
     );
     balls.push(ball);
   }
+  man.draw();
+  man.update();
   for(let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
@@ -108,7 +129,8 @@ function loop() {
       balls.splice(i, 1)
     };
   }
-
+  // Hægt og rólega leyfir fleiri object að vera til
+  max += 0.01;
   requestAnimationFrame(loop);
 }
 
